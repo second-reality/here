9c840f4d2fb077c09e5bea1ee94a0beb750a3aed
Pierrick Bouvier <pierrick.bouvier@linaro.org>
2024-05-23 15:26:47 -0700

    download and display tile

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
+use image::io::Reader as ImageReader;
 use rand::Rng;
 use slint::*;
-use ureq::*;
 
 slint::slint! {
     export component MainWindow inherits Window {
@@ -12,28 +12,43 @@ slint::slint! {
 }
 
 fn get_tile() -> Image {
-    let mut buffer = SharedPixelBuffer::<Rgb8Pixel>::new(256, 256);
-    let body: String = ureq::get("https://tile.openstreetmap.org/13/1294/2788.png")
-        .call().unwrap().into_string().unwrap();
-    println!("{}", body);
-    Image::from_rgb8(buffer)
+    let mut buf = Vec::new();
+    let _ = ureq::get("https://tile.openstreetmap.org/13/1294/2788.png")
+        .call()
+        .unwrap()
+        .into_reader()
+        .read_to_end(&mut buf);
+
+    let tile = ImageReader::new(std::io::Cursor::new(buf))
+        .with_guessed_format()
+        .unwrap()
+        .decode()
+        .unwrap()
+        .into_rgba8();
+    let buffer = SharedPixelBuffer::<Rgba8Pixel>::clone_from_slice(
+        tile.as_raw(),
+        tile.width(),
+        tile.height(),
+    );
+    Image::from_rgba8(buffer)
 }
 
 fn build_map(w: u32, h: u32, aim: u8) -> Image {
     let tile = get_tile();
-    let mut pixel_buffer = SharedPixelBuffer::<Rgb8Pixel>::new(w, h);
+    tile
+    //let mut pixel_buffer = SharedPixelBuffer::<Rgb8Pixel>::new(256, 256);
 
-    for (i, pixel) in pixel_buffer.make_mut_slice().iter_mut().enumerate() {
-        let x = i % w as usize;
-        let val = 255 - (((x * aim as usize) / w as usize) as u8);
-        *pixel = Rgb8Pixel {
-            r: val,
-            g: val,
-            b: val,
-        };
-    }
+    //for (i, pixel) in pixel_buffer.make_mut_slice().iter_mut().enumerate() {
+    //    let x = i % w as usize;
+    //    let val = 255 - (((x * aim as usize) / w as usize) as u8);
+    //    *pixel = Rgb8Pixel {
+    //        r: val,
+    //        g: val,
+    //        b: val,
+    //    };
+    //}
 
-    Image::from_rgb8(pixel_buffer)
+    //Image::from_rgb8(pixel_buffer)
 }
 
 fn main() {
